21949aa70b0fa08d07fdde88ecfbb36f87b72e4b
MyTalented <41499092+MyTalentedAdmin@users.noreply.github.com>
2019-01-22 17:57:14 -0500

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,3 +1,4 @@
+console.error("[DISCORD API] Found one fault: The guild ID provided for the widget was invalid or the widget has been disabled.");
 console.log("Connecting to the API...");
 console.log("Successfully connected...");
 console.log("If you're the site owner, please visit localhost:8271 to monitor the API logs...");
